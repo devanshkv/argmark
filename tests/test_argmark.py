4b383ca6a88642c580513f38f37a73b1aaf10666
Devansh Agarwal <devansh.kv@gmail.com>
2020-08-18 20:29:14 -0400

    More tests, more coverage

--- tests/test_argmark.py
+++ tests/test_argmark.py
@@ -1,4 +1,5 @@
 import filecmp
+import sys
 
 from argmark.argmark import *
 
@@ -18,7 +19,22 @@ def test_gen_help():
     py_file = os.path.join(_install_dir, "sample_argparse.py")
     md_file = "sample_argparse.md"
     answer = os.path.join(_install_dir, "answer.md")
-    with open(py_file, 'r') as f:
+    with open(py_file, "r") as f:
+        gen_help(f.readlines())
+    assert os.path.isfile(md_file)
+    assert filecmp.cmp(md_file, answer)
+    os.remove(md_file)
+
+
+def test_main():
+    py_file = os.path.join(_install_dir, "sample_argparse.py")
+    sys.argv.append("-f")
+    sys.argv.append(py_file)
+    sys.argv.append("-v")
+    main()
+    md_file = "sample_argparse.md"
+    answer = os.path.join(_install_dir, "answer.md")
+    with open(py_file, "r") as f:
         gen_help(f.readlines())
     assert os.path.isfile(md_file)
     assert filecmp.cmp(md_file, answer)
